169632d2f56f29b0d0eccda878bd0694c0e6ab40
nt0n1 <ivan.benegiamo@gmail.com>
2014-02-04 00:27:39 +0100

    shop servelet e some beans

---
+++ Ebook_Shop/src/eshop/ShopServlet.java
@@ -0,0 +1,48 @@
+package eshop;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ShopServlet
+ */
+@WebServlet("/ShopServlet")
+public class ShopServlet extends HttpServlet implements javax.servlet.Servlet{
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ShopServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    public void init(ServletConfig config) throws ServletException{
+    	System.out.println("*** initializing controller servlet");
+    	super.init(config);
+    	
+    	
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ Ebook_Shop/src/eshop/model/DataManager.java
@@ -0,0 +1,5 @@
+package eshop.model;
+
+public class DataManager {
+
+}
